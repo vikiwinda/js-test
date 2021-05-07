a769287fe277c91677d9a81c038ba8309a57e805
Vikita Windarwati <vikita.windarwati@gmail.com>
2021-05-07 14:55:10 +0700

    submitting task 1 and task 2 first

---
+++ test4.js
@@ -0,0 +1,67 @@
+// Expected Result : 
+// [
+//   { name: 'Rahman', school_name: 'STJK' },
+//   { name: 'Budi', school_name: 'STJK' }
+// ]
+// Direction :
+// Return a formatted array for students of specific school using id of school.
+const students = [
+  {
+    id: "1",
+    name: "Ryan",
+    school: {
+      id: "1",
+      short_name: "PLMK"
+    },
+  },
+  {
+    id: "2",
+    name: "Ari",
+    school: {
+      id: "1",
+      short_name: "PLMK"
+    },
+  },
+  {
+    id: "3",
+    name: "Rahman",
+    school: {
+      id: "2",
+      short_name: "STJK"
+    },
+  },
+  {
+    id: "4",
+    name: "Budi",
+    school: {
+      id: "2",
+      short_name: "STJK"
+    },
+  },
+  {
+    id: "5",
+    name: "Jaka",
+    school: {
+      id: "1",
+      short_name: "PLMK"
+    },
+  }
+]
+const schoolId = '2';
+
+function result(students, schoolId) {
+  // Your Code Here
+  // for (let data in students) {
+      // console.log(parseInt(students[data].school.id))
+    const result = students
+        .filter(student_info => {
+          return student_info.school.id == schoolId;
+        })
+        .map(student_info=> ({ 
+          name: student_info.name,
+          school_name: student_info.school.short_name
+        }))
+  return result
+}
+
+console.log(result(students, schoolId));
